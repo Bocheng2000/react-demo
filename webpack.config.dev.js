7a47dda89879e1ec95b235be79b4dbd8e3466591
chuchengbo <15656205870@163.com>
2017-01-04 17:13:29 +0800

    first

---
+++ webpack.config.dev.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const autoprefixer = require('autoprefixer');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    context: path.resolve("./src"),
+
+    entry: {//处理入口
+        vendor: [],
+        index:["./index/index.js"],
+    },
+
+    output: {
+        path: path.resolve("./public/assets"),
+        publicPath: "/assets/",
+        filename: "[name]/bundle.js"
+    },
+
+    resolve: {
+        extensions: [ "", ".js", ".less" ]
+    },
+
+    devtool: "source-map",
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,            //需要处理的文件
+                loaders: [ "babel" ],       //
+                include: path.join(__dirname, 'src/')
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader!less-loader")
+            }
+        ]
+    },
+
+    postcss: [ autoprefixer() ],
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+
+        new webpack.NoErrorsPlugin(),
+
+        new webpack.ProvidePlugin({
+            
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            filename: "vendor.js",
+            minChunks: Infinity
+        }),
+
+        new ExtractTextPlugin("./[name]/resource/bundle.css")
+    ],
+
+//替换需要请求的数据接口
+    devServer: { 
+        proxy: { 
+                "/api/*": { 
+                "target": { 
+                "host": "music.163.com", 
+                "protocol": 'http:', 
+                "port": 80 
+                }, 
+            ignorePath: false, 
+            changeOrigin: true, 
+            secure: false, 
+                 // headers: { 
+                 // "Referer": "http://music.163.com" 
+                 // } 
+                } 
+            } 
+        }
+
+};
